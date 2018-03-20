9526c56abb0623cc5d9a6316c80e911c3236a6da
Timotej Kos <timppa@timotejs-macbook-pro.local>
2018-03-20 18:56:55 +0200

    Add new item functionality.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,13 +10,15 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Todo Item 1", "Todo Item 2", "Todo Item 3"]
+    var itemArray = ["Todo Item 1", "Todo Item 2", "Todo Item 3"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    
+    //MARK - UITableViewController datasource methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -28,6 +30,7 @@ class TodoListViewController: UITableViewController {
         return cell
     }
     
+    //MARK - UITableViewController delegate methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
@@ -37,6 +40,43 @@ class TodoListViewController: UITableViewController {
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK - Add new items to todo list
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        // Variable to hold textField text
+        var textField = UITextField()
+        
+        // Create alert controller
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        
+        // Create "button" with action for alert controller
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            //What happens when user clicks Add Item button in alert popup
+            self.itemArray.append(textField.text!)
+            
+            // Re-render view with new data
+            self.tableView.reloadData()
+            
+        }
+        
+        // Closure gets triggered when textField is initialized in alert
+        alert.addTextField { (alertTextField) in
+            
+            alertTextField.placeholder = "Insert text"
+            // Local alertTextField exist only in this closure,
+            // that is why we create reference to local variable at initialization
+            textField = alertTextField
+        }
+        
+        // Add button to controller
+        alert.addAction(action)
+        
+        // Show controller
+        present(alert, animated: true, completion: nil)
+        
     }
     
 }
